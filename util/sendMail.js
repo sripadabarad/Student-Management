b08154ac1dc65590377896d8e15ef090d1fcbc7d
sripada barad <sripadabarad555@gmail.com>
2026-03-01 17:51:17 +0530

    Add a sendMail in util folder for reusable in email and downloaded a npm package nodemailer

---
+++ util/sendMail.js
@@ -0,0 +1,49 @@
+
+const nodemailer = require("nodemailer");
+const customError = require("./customError");
+
+
+//setup the nodemailer for send mail
+
+const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,    // smtp gamil.com
+    port: Number(process.env.SMTP_PORT),    // 587   // i used Number bcause that is string
+    secure: process.env.SMTP_SECURE === "true",
+    auth:{
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS, 
+    },
+});
+
+
+// now create a sendemail  async fucntion for send the email and the object parameter
+
+const sendEmail = async ({ to,subject,html})=>{
+    
+    // Now validation the parameter
+
+    if(!to || !subject || !html ){
+        throw new customError("All fildes are require",400);
+    };
+
+    try {
+         
+        // agin a function to send the parameters
+        const info = await transporter.sendMail({
+            from: process.env.EMAIL_FROM,  //sending email from
+            to,
+            subject,
+            html,
+        });
+
+        return info ;
+
+    } catch (error) {
+
+        console.error("Email error",error.message);
+        
+        throw new customError("Email sending failed",500);
+    }
+};
+
+module.exports = sendEmail ;
