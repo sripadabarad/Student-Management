70ad6d94d53c9b68600a27887aa057b65174f1be
sripada barad <sripadabarad555@gmail.com>
2026-03-07 20:12:39 +0530

    created route file and added updated  the end points

---
+++ routes/authRoutes.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const router = express.Router();
+
+// add all controller data
+const 
+{ register, 
+ login ,
+ refresh_Token ,
+ logOut ,
+ changePassword ,
+ forgotPassword ,
+ resetPassword} = require("../controller/authController");
+
+ // auth middleware for protected routes
+
+ const authentication = require("../middleware/authentication.js");
+
+ //now RABC for specific person 
+ const authorization = require("../middleware/authorization.js");
+
+
+ //NOW THE ROUTES END POINTS
+ 
+ //Register
+ router.post("/register",register);
+
+ //login
+ router.post("/login",login);
+
+ //refresh_Token
+ router.post("/refresh-Token",refresh_Token);
+
+ //logout
+ router.post("/logout",authentication,logOut);
+
+ //changepassword
+ router.post("/change-password",authentication ,changePassword);
+
+ //forgotpassword
+ router.post("/forgot-password",forgotPassword);
+
+ //resetpassword
+ router.post("/reset-password",resetPassword);
+
+ module.exports = router;
+
