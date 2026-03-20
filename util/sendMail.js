cb56ec2326429546615f88667b773bc7169b2298
sripada barad <sripadabarad555@gmail.com>
2026-03-20 06:40:07 +0530

    added production ready sendEmail utility with error handling by using sendGrid

--- util/sendMail.js
+++ util/sendMail.js
@@ -1,4 +1,9 @@
 const sgMail = require("@sendgrid/mail");  // sg = sendgrid , Mail = email
+const customError = require("../util/customError");
+
+if(!process.env.SENDGRID_API_KEY){
+    throw new customError("SENDGRID_API_KEY is not defined",400)
+}
 
 //set API Key
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
@@ -6,17 +11,26 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 //reusable fuction
 
-const sendEmail = async({ to , subject, html}) => {
+const sendEmail = async({ to , subject, html, text = "" }) => {
+    if(!to || !subject || !html){
+        throw new customError("Missing required email fields",400)
+    }
  
     const msg = {
         to,
         from :process.env.EMAIL_FROM,
         subject,
         html,
+        text
     };
 
-    await sgMail.send(msg);  // yehi send grid use hua 
-
+    try {
+         await sgMail.send(msg);  // yehi send grid use hua 
+         return {success: true};
+    } catch (error) {
+        console.error("SendGrid Error:", error.response?.body || error.message);
+        throw new customError("Email sending failed",500);
+    }
 };
 
 
