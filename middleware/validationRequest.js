315b8cc1a1b6d5a127cdfcd24e7b2bb3929a5d80
sripada barad <sripadabarad555@gmail.com>
2026-03-23 03:02:08 +0530

    update validation

--- middleware/validationRequest.js
+++ middleware/validationRequest.js
@@ -29,29 +29,3 @@ const validateRequest = (req,res,next) =>{
 
 module.exports = validateRequest ;
 
-
-
-// const { validationResult } = require("express-validator");
-
-// const validateRequest = (req, res, next) => {
-    
-//     const errors = validationResult(req);
-
-//     if (!errors.isEmpty()) {
-
-//         const formattedErrors = errors.array().map(err => ({
-//             field: err.path,
-//             message: err.msg
-//         }));
-
-//         return res.status(400).json({
-//             success: false,
-//             message: "Validation failed",
-//             errors: formattedErrors
-//         });
-//     }
-
-//     next();
-// };
-
-// module.exports = validateRequest;
