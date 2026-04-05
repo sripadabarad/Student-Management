5e7000d7bf45f8327829e84fcc56af003c59cdeb
sripada barad <sripadabarad555@gmail.com>
2026-04-06 01:44:43 +0530

    add token time 15m or accestoken

--- util/token.js
+++ util/token.js
@@ -4,7 +4,7 @@ const generateAccessToken = (user)=>{
     return jwt.sign(
         {id:user._id.toString(), role:user.role},
         process.env.JWT_ACCESS_TOKEN,
-        {expiresIn:"2m"}
+        {expiresIn:"15m"}
     )
 };
 
